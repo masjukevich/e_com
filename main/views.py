92c5128594055e0fc47078d8e467a68145590e57
masu <masjukevich@gmail.com>
2024-01-25 13:14:44 +0300

    (main) add templates + styles

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    context = {
+        'title': 'Мебель здесь',
+    }
+
+    return render(
+        request,
+        'main/index.html',
+        context
+    )
